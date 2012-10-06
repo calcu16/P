019114772c6554daadd01f678862dbdc962b593b
Andrew Carter <acarter@cs.hmc.edu>
2012-10-06 12:41:28 -0700

    The packrat parser now uses shared variables

--- src/Packrat/ast.cpp
+++ src/Packrat/ast.cpp
@@ -38,86 +38,64 @@ using namespace packrat;
 const string AST::NO_STRING = "";
 const AST AST::NO_AST;
 
-template<typename T>
-static T* copyp(T* t)
+AST::AST_::AST_(int start, int end, int cost)
+    : value_(nullptr), start_(start), end_(end), cost_(cost)
 {
-    return t == NULL ? (T*)NULL : new T(*t);
+    // Nothing else to do
+}
+
+AST::AST_::AST_(int start, int end, const string& value, int cost)
+    : value_(make_shared<string>(value)), start_(start), end_(end), cost_(cost)
+{
+    // Nothing else to do
+}
+
+AST::AST(AST_t tree)
+    : tree_(tree)
+{
+  // Nothing else to do
 }
 
 AST::AST(int start)
-    : value_(NULL), start_(start), end_(-1), cost_(0)
+    : tree_(new AST_(start,-1))
 {
-    // Nothing else to do
+  // Nothing else to do
 }
 
 AST::AST(int start, int end, int cost)
-    : value_(NULL), start_(start), end_(end), cost_(cost)
+    : tree_(new AST_(start, end, cost))
 {
-    // Nothing else to do
+  // Nothing else to do
 }
 
 AST::AST(int start, int end, const string& value, int cost)
-    : value_(new string(value)), start_(start), end_(end), cost_(cost)
+    : tree_(new AST_(start, end, value, cost))
 {
     // Nothing else to do
 }
 
 AST::AST(const AST& left, const AST& right)
-    : value_(NULL), start_(left.start_), end_(right.end_),
-        cost_(left.cost_ + right.cost_)
+    : tree_(new AST_(left.tree_->start_, right.tree_->end_,
+                     left.tree_->cost_ + right.tree_->cost_))
 {
     AST fl = left.flatten(), rl = right.flatten();
-    for(unordered_map<string,AST*>::iterator i = fl.assoc_.begin();
-                i != fl.assoc_.end(); ++i)
-        assoc_.insert(pair<string,AST*>(i->first,copyp(i->second)));
-    for(unordered_map<string,AST*>::iterator i = rl.assoc_.begin();
-                i != rl.assoc_.end(); ++i)
-        assoc_.insert(pair<string,AST*>(i->first,copyp(i->second)));
-    if(fl.value_ && rl.value_)
-        value_ = new string(*fl + *rl);
+    for(auto i = fl.tree_->assoc_.begin(); i != fl.tree_->assoc_.end(); ++i)
+        tree_->assoc_.insert(pair<string,AST>(i->first,i->second));
+    for(auto i = rl.tree_->assoc_.begin(); i != rl.tree_->assoc_.end(); ++i)
+        tree_->assoc_.insert(pair<string,AST>(i->first,i->second));
+    if(fl.tree_->value_ && rl.tree_->value_)
+        tree_->value_ = shared_ptr<string>(new string(*fl + *rl));
 }
 
 AST::AST(const string& name, const AST& other)
-    : value_(NULL), start_(other.start_), end_(other.end_), cost_(other.cost_)
+    :tree_(new AST_(other.tree_->start_, other.tree_->end_, other.tree_->cost_))
 {
     (*this)[name] = other;
 }
 
-AST::AST(const AST& copy)
-    : value_(copyp(copy.value_)), start_(copy.start_), end_(copy.end_),
-        cost_(copy.cost_)
-{
-    for(unordered_map<string, AST*>::const_iterator i = copy.assoc_.begin();
-            i != copy.assoc_.end(); ++i)
-        assoc_.insert(pair<string,AST*>(i->first,copyp(i->second)));
-    for(AST::const_iterator i = copy.begin(); i != copy.end(); ++i)
-        numbered_.push_back(new AST(*i));
-}
-
-AST::~AST()
-{
-    for(unordered_map<string, AST*>::iterator i = assoc_.begin();
-            i != assoc_.end(); ++i)
-        delete i->second;
-    for(numbered_t::iterator i = numbered_.begin(); i != numbered_.end(); ++i)
-        delete *i;
-    delete value_;
-}
-
 void AST::swap(AST& other)
 {
-    std::swap(assoc_, other.assoc_);
-    std::swap(numbered_, other.numbered_);
-    std::swap(value_, other.value_);
-    std::swap(start_, other.start_);
-    std::swap(end_, other.end_);
-    std::swap(cost_, other.cost_);
-}
-
-AST& AST::operator=(AST rhs)
-{
-    swap(rhs);
-    return *this;
+  std::swap(tree_, other.tree_);
 }
 
 AST AST::operator()(const std::string& name) const { return AST(name, *this); }
@@ -126,13 +104,13 @@ AST AST::operator<<(const AST& rhs) const
 {
     if(!rhs)
         return *this;
-    AST out(start_, rhs.end_, cost_ + rhs.cost_);
-    if(size() > 0 || (value_ != NULL && *value_ != ""))
-        out.numbered_.push_back(new AST(*this));
+    AST out(tree_->start_, rhs.tree_->end_, tree_->cost_ + rhs.tree_->cost_);
+    if(size() > 0 || (tree_->value_ != NULL && *tree_->value_ != ""))
+        out.tree_->numbered_.push_back(*this);
     else
-        for(AST::const_iterator i = begin(); i != end(); ++i)
-            out.numbered_.push_back(new AST(*i));
-    out.numbered_.push_back(new AST(rhs));
+        for(auto i = begin(); i != end(); ++i)
+            out.tree_->numbered_.push_back(*i);
+    out.tree_->numbered_.push_back(rhs.tree_);
     return out;
 }
 
@@ -145,15 +123,15 @@ AST& AST::operator<<=(const AST& rhs)
 
 AST AST::operator>>(const AST& rhs) const
 {
+    AST out(tree_->start_, rhs.tree_->end_, tree_->cost_ + rhs.tree_->cost_);
     if(!*this)
         return rhs;
-    AST out(start_, rhs.end_, cost_ + rhs.cost_);
-    out.numbered_.push_back(new AST(*this));
-    if(rhs.size() > 0 || (rhs.value_ != NULL && *rhs.value_ != ""))
-        out.numbered_.push_back(new AST(rhs));
+    out.tree_->numbered_.push_back(*this);
+    if(rhs.size() > 0||(rhs.tree_->value_ != NULL && *rhs.tree_->value_ != ""))
+        out.tree_->numbered_.push_back(rhs);
     else
-        for(AST::const_iterator i = rhs.begin(); i != rhs.end(); ++i)
-            out.numbered_.push_back(new AST(*i));
+        for(auto i = rhs.begin(); i != rhs.end(); ++i)
+            out.tree_->numbered_.push_back(*i);
     return out;
 }
 
@@ -167,7 +145,7 @@ AST& AST::operator>>=(const AST& rhs)
 AST AST::operator+(const AST& rhs) const
 {
     if(!(*this && rhs))
-        return AST(start_);
+        return AST(tree_->start_);
     return AST(*this, rhs);
 }
 
@@ -183,37 +161,37 @@ AST AST::flatten() const
 {
     if(length() == 0)
         return *this;
-    AST acc(start_, start_, "");
-    for(AST::const_iterator i = begin(); i != end(); ++i)
+    AST acc(tree_->start_, tree_->start_, "");
+    for(auto i = begin(); i != end(); ++i)
         acc = AST(acc, *i);
-    acc.cost_ = cost_;
+    acc.tree_->cost_ = tree_->cost_;
     return acc;
 }
 
 
 const string& AST::operator*() const
 {
-    return value_ == NULL ? NO_STRING : *value_;
+    return tree_->value_ == nullptr ? NO_STRING : *tree_->value_;
 }
 
 string& AST::operator*()
 {
-    if(value_ == NULL)
-        value_ = new string();
-    return *value_;
+    if(tree_->value_ == nullptr)
+        tree_->value_ = shared_ptr<string>(new string());
+    return *tree_->value_;
 }
 
 
 AST& AST::operator[](size_t index)
 {
     while(index >= length())
-        numbered_.push_back(new AST());
-    return *numbered_[index];
+        tree_->numbered_.push_back(AST());
+    return tree_->numbered_[index];
 }
 
 const AST& AST::operator[](size_t index) const
 {
-    return index >= length() ? NO_AST : *numbered_[index];
+    return index >= length() ? NO_AST : tree_->numbered_[index];
 }
 
 AST& AST::operator[](int index)
@@ -226,20 +204,20 @@ const AST& AST::operator[](int index) const
     return (*this)[(size_t)index];
 }
 
-size_t AST::length() const { return numbered_.size(); }
+size_t AST::length() const { return tree_->numbered_.size(); }
 
 AST& AST::operator[](const std::string& key)
 {
-    if(assoc_.find(key) == assoc_.end())
-        assoc_.insert(pair<string,AST*>(key, new AST()));
-    return *assoc_.find(key)->second;
+    if(tree_->assoc_.find(key) == tree_->assoc_.end())
+        tree_->assoc_.insert(pair<string,AST>(key, AST()));
+    return tree_->assoc_.find(key)->second;
 }
 
 const AST& AST::operator[](const std::string& key) const
 {
-    return assoc_.find(key) == assoc_.end()
+    return tree_->assoc_.find(key) == tree_->assoc_.end()
             ? NO_AST
-            : *assoc_.find(key)->second;
+            : tree_->assoc_.find(key)->second;
 }
 
 AST& AST::operator[](const char* key)
@@ -252,56 +230,56 @@ const AST& AST::operator[](const char* key) const
     return (*this)[string(key)];
 }
 
-size_t AST::size() const { return assoc_.size(); }
+size_t AST::size() const { return tree_->assoc_.size(); }
 
-AST::operator bool() const { return end_ != -1; }
-bool AST::operator!() const { return end_ == -1; }
-int AST::startc() const { return start_; }
-int AST::endc() const { return end_; }
-int AST::cost() const { return cost_; }
+AST::operator bool() const { return tree_->end_ != -1; }
+bool AST::operator!() const { return tree_->end_ == -1; }
+int AST::startc() const { return tree_->start_; }
+int AST::endc() const { return tree_->end_; }
+int AST::cost() const { return tree_->cost_; }
 
 AST::ConstIterator AST::begin() const
 {
-    return ConstIterator(numbered_.begin());
+    return tree_->numbered_.begin();
 }
 
 AST::Iterator AST::begin()
 {
-    return Iterator(numbered_.begin());
+    return tree_->numbered_.begin();
 }
 
 AST::ConstIterator AST::end() const
 {
-    return ConstIterator(numbered_.end());
+    return tree_->numbered_.end();
 }
 
 AST::Iterator AST::end()
 {
-    return Iterator(numbered_.end());
+    return tree_->numbered_.end();
 }
 
-ostream& AST::print(ostream& out, string tab) const
+ostream& AST::AST_::print(ostream& out, string tab) const
 {
     string ntab = tab + "\t";
     out << tab << "#" << cost_ << endl;
-    if(size())
-        for(unordered_map<string, AST*>::const_iterator j = assoc_.begin();
-                    j != assoc_.end(); ++j)
-            j->second->print(out << tab << j->first << endl, ntab);
-    else if(length())
+    if(assoc_.size() > 0)
+        for(auto j = assoc_.begin(); j != assoc_.end(); ++j)
+            j->second.tree_->print(out << tab << j->first << endl, ntab);
+    else if(numbered_.size() > 0)
     {
         size_t i = 0;
-        for(AST::const_iterator j = begin(); j != end(); ++j, ++i)
-            j->print(out << tab << i << endl, ntab);
+        for(auto j = numbered_.begin(); j != numbered_.end(); ++j, ++i)
+            j->tree_->print(out << tab << i << endl, ntab);
     }
-    else if(value_ != NULL)
+    else if(value_ != nullptr)
         out << tab << ": \"" << *value_ << '"' << endl;
     return out;
 }
 
 ostream& AST::print(ostream& out) const
 {
-    return print(out << endl << "<AST " << start_ << " - " << end_ << endl, "")
+    return tree_->print(out << endl << "<AST " << tree_->start_
+                            << " - " << tree_->end_ << endl, "")
                 << ">" << endl;
 }
 
